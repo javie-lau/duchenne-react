2d154b8d7e96649dc96eb16015b8c6ea8d2e5694
luisaromero <luisa.marcela.romero@gmail.com>
2019-08-05 17:32:57 -0400

    subiendo responsive a cards

--- dsg-duchenne/src/components/Cards/Cards.jsx
+++ dsg-duchenne/src/components/Cards/Cards.jsx
@@ -25,9 +25,7 @@ class Cards extends Component{
   <div className="card col-md-4 col-12">
       <img src="https://f1.media.brightcove.com/8/3850378299001/3850378299001_5537454316001_5537448372001-vs.jpg?pubId=3850378299001&videoId=5537448372001" class="card-img-top" alt="..."></img>
       <div className="card-body">
-      {/* <h5 className="card-title">Noticia</h5> */}
       <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-      {/* <p className="card-text"><small className="text-muted">Last updated 3 mins ago</small></p> */}
       <a className="btn-more" href="#" > > Leer más</a>
     </div>
   </div>
