41f106f7d61eb11787b471bb00b55cf9ebffe1b2
Mariia <maria.bookvoed@gmail.com>
2019-08-09 18:38:15 -0400

    dropdown hover

--- dsg-duchenne/src/views/Sumate.jsx
+++ dsg-duchenne/src/views/Sumate.jsx
@@ -11,33 +11,48 @@ class Sumate extends Component {
         return(
             <React.Fragment>
                 <Navbar/>
-                  <div className="container" id="sumate">
-                       <h4>Tu colaboración es fundamental para seguir luchando contra la distrofia Muscular de Duchenne</h4>
-                        <p>Con tu ayuda podemos financiar y promover proyectos de investigación para estar cada día más cerca de la cura de esta enfermedad.¡Gracias por unirte a nuestra fundación y ser parte de la solución!</p>
-              
-                    <div className="row">
-                        <div className="col-6 col-md-3 offset-md-4">
-                            <img className="img-fluid" src={images['superman-svg.png']} alt=""/>
-                        </div>
-                        <div className="col-6 col-md-3">
-                            <img className="img-fluid" src={images['enterprise-svg-svg.png']} alt=""/>
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-6 col-md-3 offset-md-4">
-                            <h6>
-                                Persona:
-                            </h6> 
-                            <Link className="link" to="/contact"><ButtonDonate/></Link>
-                        </div>
-                        <div className="col-6 col-md-3">
-                            <h6>
-                                Empresa solidaria:
-                            </h6>
-                            <Link className="link" to="/contact"><ButtonDonate/></Link>
+                    <div className="container" id="sumate">
+                        <div className="row">
+                            <div className="col-md-8 offset-md-3 offset-1 col-10">
+                                <h4>Tu colaboración es <span>fundamental</span> para seguir luchando contra  <br/>la Distrofia Muscular de Duchenne.</h4>
+                            </div>
                         </div> 
+                        <div className="row">
+                            <div className="col-md-8 offset-md-3 offset-1 col-10">
+                                <p>Gracias a tu ayuda podemos financiar y promover proyectos de investigación para estar cada día mas cerca de la cura de esta terrible enfermedad. Gracias por unirte a nuestra fundación y ser parte de la solución.</p>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-md-4 offset-md-3">
+                                <img className="img-fluid center" src={images['superman-svg.png']} alt=""/>
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <img className="img-fluid center" src={images['enterprise-svg-svg.png']} alt=""/>
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-6 col-md-4 offset-md-3">
+                                <h4>
+                                    Persona:
+                                </h4> 
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <h4>
+                                    Empresa solidaria:
+                                </h4>
+                            </div>       
+                        </div>     
+                        <div className="row"> 
+                            <div className="col-6 col-md-4 offset-md-3">
+                                <Link className="link center" to="/contact"><ButtonDonate/></Link>
+                            </div>
+                            <div className="col-6 col-md-4">
+                                <Link className="link center" to="/contact"><ButtonDonate/></Link>
+                            </div>
+                        </div>  
+
                     </div>
-                </div>
+                
                 <Footer />
             </React.Fragment>
         )
